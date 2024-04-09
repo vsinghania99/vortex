3cd4646edd75b196ce43c3762adf4399d2fe6912
Varsha Singhania <vsinghania8@gatech.edu>
2024-04-09 09:37:17 -0400

    Fixed bugs for higher matrix sizes [TODO : cleanup debug prints]

--- sim/simx/execute.cpp
+++ sim/simx/execute.cpp
@@ -2322,7 +2322,7 @@ void Warp::execute(const Instr &instr, pipeline_trace_t *trace) {
       num_data_per_thread = 1;
     }
   
-    uint32_t data_bytes_load = mem_bytes*num_data_per_thread;
+    uint32_t data_bytes_load = mem_bytes*num_data_per_thread*n_tiles;
     uint32_t data_bytes_store = (mem_bytes*num_data_per_thread);
 
     DP(3, "Num Tiles=" << n_tiles << std::endl);
@@ -2362,7 +2362,8 @@ void Warp::execute(const Instr &instr, pipeline_trace_t *trace) {
               //csr-> scratchpad (TODO :: can intermediate step of moving to CSR be skipped?)
 
               scratchpad[loop_offset + (immsrc*(n_tiles)*tc_size*tc_size) + (t*num_data_per_thread) + n] = core_->get_csr(csr_addr[(immsrc*num_data_per_thread) + n], t, warp_id_);
-              //DP(3, "Scratchpad Index: " << loop_offset + (immsrc*(n_tiles)*tc_size*tc_size) + (t*num_data_per_thread) + n << ", Value: " << scratchpad[loop_offset + (immsrc*(n_tiles)*tc_size*tc_size) + (t*num_data_per_thread) + n]);
+              DP(3, "imm = " << immsrc);
+              DP(3, "Scratchpad Index: " << loop_offset + (immsrc*(n_tiles)*tc_size*tc_size) + (t*num_data_per_thread) + n << ", Value: " << scratchpad[loop_offset + (immsrc*(n_tiles)*tc_size*tc_size) + (t*num_data_per_thread) + n]);
             }
             loop_offset += tc_size*tc_size;
           }
@@ -2430,9 +2431,11 @@ void Warp::execute(const Instr &instr, pipeline_trace_t *trace) {
                 for (int j = 0; j < tc_size; j++) { //COL-2
                   int sum = 0;
                   for (int k = 0; k < tc_size; k++){ //COL-1
+                    DP(3, "Multiplying: A index = " << loop_offset + i * tc_size + k << "; B index = " << loop_offset + n_tiles*tc_size*tc_size + (k * tc_size + j));
                     sum = sum + scratchpad[loop_offset + i * tc_size + k] *scratchpad[loop_offset + n_tiles*tc_size*tc_size + (k * tc_size + j)];
                   }
                   scratchpad[(n_tiles*tc_size*tc_size*2) + (i * tc_size + j)] += sum; //[i * col2 + j] = sum
+                  DP(3, "Scratchpad Index: " << (n_tiles*tc_size*tc_size*2) + (i * tc_size + j) << " , Value=" << scratchpad[(n_tiles*tc_size*tc_size*2) + (i * tc_size + j)]);
                 }
               }
               loop_offset += tc_size*tc_size; //Move to the next tiled matmul fragment
